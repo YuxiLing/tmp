6927c754b8ac507c9284ff055c8dc3bfd05dfef1
YuxiLing <lingyuxi057@gmail.com>
2022-03-17 18:01:55 +0800

    Update get_callee.js

--- get_callee.js
+++ get_callee.js
@@ -21,45 +21,46 @@ function begin() {
         return results;
     }
 
-    function traverse_file(dir) {
+    function traverse_file(dir,handle_ast_file) {
         var js_file = walk(dir);
 
         js_file.forEach(function (p) {
             if (p.slice(-3) == ".js") {
                 // console.log(p);
                 var output = p.slice(0, -3) + '_temp.json'
-                handle_ast_file(p, output);
+                handle_ast_file(p, output,traverse);
             }
         });
 
         return;
     }
-    function handle_ast_file(input_file, output_file) {
-        fs.readFile(input_file, { encoding: 'utf-8' }, function (err, data) {
-            try {
-                esprima.parseScript(data, {}, function (node, meta) {
-                    let reached_node = []
-                    let called_api = []
+    function handle_ast_file(input_file, output_file,traverse) {
+        var data= fs.readFileSync(input_file, { encoding: 'utf-8' });
+        
+        try {
+            var node=esprima.parseScript(data);
+            
+            var reached_node = []
+            var called_api = []
 
-                    called_api = traverse(node, reached_node, called_api);
-                    // console.log(called_api);
+            traverse(node, reached_node, called_api,handle_ast_node);
+            console.log(called_api);
 
-                });
-                success = success + 1;
-                console.log(success);
-            } catch (e) {
-                // console.error('skip a file' + input_file);
-                // console.log(e);
-                failed = failed + 1;
-            }
-        });
+            success = success + 1;
+            console.log(success);
+        } catch (e) {
+            // console.error('skip a file' + input_file);
+            // console.log(e);
+            failed = failed + 1;
+        }
+        
         // var code = fs.readFile(input_file, {encoding: 'utf-8'});
         // var ast = esprima.parseScript(code);
         // traverse(ast,handle_ast_node);
     }
-    function traverse(node, reached_node, called_api) {
-        let new_called_api = handle_ast_node(node, reached_node, called_api)
-        called_api = new_called_api;
+    function traverse(node, reached_node, called_api,handle_ast_node) {
+        handle_ast_node(node, reached_node, called_api)
+        
         // console.log('inside '+called_api);
 
         for (var key in node) { //2
@@ -70,26 +71,25 @@ function begin() {
                     if (Array.isArray(child)) {
                         child.forEach(function (node) { //5
                             reached_node.push(node);
-                            new_called_api = traverse(node, reached_node, called_api)
-                            called_api = new_called_api
+                            traverse(node, reached_node, called_api,handle_ast_node)
 
                             reached_node.pop();
 
                         });
                     } else {
                         reached_node.push(child);
-                        new_called_api = traverse(child, reached_node, called_api)
-                        called_api = new_called_api;
+                        traverse(child, reached_node, called_api,handle_ast_node)
+                        
                         //6
                         reached_node.pop();
                     }
                 }
             }
         }
-        return called_api;
+        // return called_api;
     }
 
-    let handle_ast_node = function (node, reached_node, called_api) {
+    function handle_ast_node(node, reached_node, called_api) {
         // console.log('handle ast node '+called_api);
         if (node.type == "CallExpression" && node.callee.type == "MemberExpression" && node.callee.object.object.name == "chrome") {
             //this is a start of a chrome API calle
@@ -97,19 +97,21 @@ function begin() {
             var api_name = node.callee.object.object.name + "." + node.callee.object.property.name + "." + node.callee.property.name;
             var api_argument = node.arguments;
             // called_api.push(JSON.stringify({"name":api_name,"argument":api_argument}));
-            called_api.push({ "name": api_name, "argument": api_argument });
-            // console.log('74'+JSON.stringify(called_api));
+            var tmp={ "name": api_name, "argument": api_argument };
+            called_api.push(tmp);
+            console.log('74'+called_api);
         } else if (node.type == "CallExpression" && node.callee.type == "MemberExpression" && node.callee.object.type == "MemberExpression" && node.callee.object.object.object.name == "chrome") {
             //this is a start of a chrome API calle
             // four layers
             var api_name = node.callee.object.object.object.name + "." + node.callee.object.object.property.name + "." + node.callee.object.property.name + "." + node.callee.property.name;
             var api_argument = node.arguments;
             // called_api.push(JSON.stringify({"name":api_name,"argument":api_argument}));
-            called_api.push({ "name": api_name, "argument": api_argument });
-            // console.log('74'+JSON.stringify(called_api));
+            var tmp={ "name": api_name, "argument": api_argument };
+            called_api.push(tmp);
+            console.log('74'+called_api);
         }
         // console.log('74'+called_api);
-        return called_api;
+        // return called_api;
 
     }
     function print_info() {
@@ -119,18 +121,12 @@ function begin() {
     }
     var success = 0;
     var failed = 0;
-    
-    print_info();
-    traverse_file('sample_ext/process');
+
+    // print_info();
+    traverse_file('sample_ext/process',handle_ast_file);
     print_info();
 }
 begin();
-// Step(
-//     traverse_file('sample_ext/process'),
-//     success++,
-//     print_info()
-// );
-
 
 // var input_file="sample_ext/process/bhghoamapcdpbohphigoooaddinpkbai/dist/import.js";
 // var output_file="";
